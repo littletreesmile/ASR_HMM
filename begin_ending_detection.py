bbc0c4380e5d8c199124e5ee38188df1dad32cc3
ye.li <ye.li@bragi.com>
2017-08-23 14:07:04 +0200

    in half way of begin_ending_detection

--- begin_ending_detection.py
+++ begin_ending_detection.py
@@ -18,28 +18,79 @@ def set_length_10ms():
         tmp_len = tmp_len - 1
     return tmp_len
 
+
+def short_time_energy(sig):
+    """ Calculate short time energy
+
+    Args:
+        sig: signals
+
+    Returns:
+        averge energy
+    """
+    abs_values = [abs(x) for x in sig]
+    return sum(abs_values)
+
+
+def zero_crossing_rate(sig):
+    """ Calculate zero crossing rate
+
+    Args:
+        sig: signals
+
+    Returns:
+        zero crossing rate
+    """
+    zero_crossing = []
+    for i in range(len(sig)-1):
+        tmp = int(abs((sig[i+1] - sig[i])) / 2)
+        zero_crossing.append(tmp)
+    return sum(zero_crossing)
+
+
 if __name__ == '__main__':
 
     file_name = 'IN_D1_FWC_S5_20170810_111645.snd'
     fs = 22050  # sampling rate
     frame_length_10ms = set_length_10ms()  # frame length in samples
-    overlap_length = int(frame_length_10ms / 2)
+    overlap_length = int(frame_length_10ms / 2)  # overlap length in samples
 
     sound_data = parse_data(file_name)
     signal_channel_a = sound_data[294000:330000, 0]
     # signal_channel_b = sound_data[294000:330000, 1]
     subsignal_len = len(signal_channel_a)
     signal_channel_b = list(range(subsignal_len))
-    num_pointing_5ms = int(subsignal_len / overlap_length)  # number of pointing by every 5ms
+
+    num_frames = int(subsignal_len / overlap_length) - 1  # number of frames by every 5ms
     current_frame = np.array([None] * frame_length_10ms)
 
 
     check = []
-    for i in range(num_pointing_5ms):
-        if i == num_pointing_5ms - 1:
-            pass
-        else:
-            current_frame[:overlap_length] = current_frame[overlap_length:]
-            current_frame[overlap_length:] = signal_channel_b[i*overlap_length:(i+1)*overlap_length]
-            check.extend(current_frame)
+    STE = []  # list of average energy
+    raw_start_found = False
+    for i in range(num_frames):
+        current_frame = signal_channel_b[i*overlap_length:i*overlap_length+frame_length_10ms]  # extract 10ms data
+        ste = short_time_energy(current_frame)  # short time energy of the current frame
+        zcr = zero_crossing_rate(current_frame)  #  zero crossing rate of the current frame
+
+        if (ste > STE_UPPER) and (not raw_start_found):
+            start_candidate = np.where(np.array(STE) < STE_LOWER)[-1]
+            if start_candidate:
+                raw_start = start_candidate[0]
+            else:
+                raw_start = i
+            raw_start_found = True
+        elif (ste < STE_LOWER) and raw_start_found:
+            raw_end = i
+
+
+
+
+
+        STE.append(ste)
+
+
+        current_frame[:overlap_length] = current_frame[overlap_length:]
+        current_frame[overlap_length:] = signal_channel_b[i*overlap_length:(i+1)*overlap_length]
+        check.extend(current_frame)
     print (check)
